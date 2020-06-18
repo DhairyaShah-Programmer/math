f5e31511a660d8f62241c609ab95438b8fa8d205
Technical Zero <65452005+TechnicalZero@users.noreply.github.com>
2020-06-18 17:00:08 +0530

    Create Polynomial_generator.py

---
+++ Polynomial_generator.py
@@ -0,0 +1,8 @@
+from time import *
+alpha = int(input("Alpha: "))
+beta = int(input("Beta: "))
+xsq = 'x\u00b2'
+mt = alpha + beta
+mml = alpha * beta
+mmll = mml ** 2
+print(str(xsq) + " + " + str(mt) + "x " + "+ " +str(mmll))
